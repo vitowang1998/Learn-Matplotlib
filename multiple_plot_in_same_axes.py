2e194e2c008f4d5cd544407288cead5a1099f058
Haoran Wang <vitowang1998@gmail.com>
2018-05-02 22:25:35 -0400

    Adds a file about ploting multiple graphs in one set of axes.

---
+++ multiple_plot_in_same_axes.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# %matplotlib inline                    if using jupyter notebook
+
+# The more pairs in x, y, the more precise the graph will be.
+x = np.linspace(0, 10, 100)
+y = x ** 3
+z = x * y / 5
+
+# There are two ways of ploting a graph, one is to use function plt.plot(a, b)
+plt.plot(x, y)
+plt.plot(x, z)
+
+plt.xlabel('Time')
+plt.ylabel('Distance')
+plt.title('Time-Distance Graph')
+plt.show()
